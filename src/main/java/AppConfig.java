bcdd6fa2d933b30dc3c821e8b3690df2fce4dbf0
celikgokhun <celik.gokhun@gmail.com>
2023-12-23 13:13:06 +0300

    initial commit

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,5 @@
+public class AppConfig {
+    public static final String sourceCompatibility = "21";
+    public static final String group = "com.celik.foodo";
+    public static final String version = "0.0.1-SNAPSHOT";
+}
