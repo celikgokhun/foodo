b5e3a0a1ad57f62a632af34f608108dd48d75dfb
celikgokhun <celik.gokhun@gmail.com>
2023-12-24 15:37:13 +0300

    complete order service domain.

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/celik/foodo/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package com.celik.foodo.order.service.domain.entity;
+
+import com.celik.foodo.domain.entity.AggregateRoot;
+import com.celik.foodo.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
